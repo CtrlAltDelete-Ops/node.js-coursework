e7ae71a5b6d0f80a09f763083106ff19e9db47af
Ismail Mohamed <ismailmohamed@Ismails-MacBook-Air.local>
2025-04-12 14:17:43 +0300

    push latest and final(for now) changes to weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,15 +2,16 @@ console.log("client side javascript file is loaded");
 
 const WeatherForm = document.querySelector("form");
 const search = document.querySelector("input");
-const errorMessage = document.querySelector("#error");
-const weatherData = document.querySelector("#data");
-const loading = document.querySelector("#loading");
+const message = document.querySelector("#message");
+
 
 WeatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
   //loading.textContent = 'Loading...'
+
+  message.textContent = 'Loading...'
 
   fetch(
     "https://api.weatherstack.com/current?access_key=484ed8ef1ce939d68cc104e7d26da522&query=" +
@@ -19,15 +20,11 @@ WeatherForm.addEventListener("submit", (e) => {
     response.json().then((data) => {
       try {
         if (data.error) {
-          errorMessage.textContent = 'Loading..'
-          return errorMessage.textContent = data.error.info;
+          return message.textContent = data.error.info;
         }
-        weatherData.textContent = 'Loading...'
-        weatherData.textContent = 'It is ' + data.current.temperature + '°C in ' + data.location.name;
-        errorMessage.textContent = "";
+        message.textContent = 'It is ' + data.current.temperature + '°C in ' + data.location.name;
       } catch (error) {
-        errorMessage.textContent = 'Loading..'
-        errorMessage.textContent = error
+        message.textContent = error
       }
     });
   });
